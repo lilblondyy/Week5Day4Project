e140a137a42cdb5bb08c2be168bfb883ca256b69
Cortney Anderson <cortney.anderson4873@stu.mtec.edu>
2024-02-22 09:51:00 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+$(".navItem").each(function () {
+  console.log(this);
+});
+
+$(".navItem").css("color", "white");
+
+$("#nav").css("background-color", "#145");
+
+$("li:even").css("background-color", "#ccc");
+
+$("li:odd").css("background-color", "#aaa");
+
+$(":input").val("Cortney");
